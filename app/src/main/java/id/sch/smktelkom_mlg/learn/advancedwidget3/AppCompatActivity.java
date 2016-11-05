ae438ae1f03dc6d8dbe05c3ecddb5d68db82ee4c
Dita Ayu <ditaayu0609@gmail.com>
2016-11-05 23:21:03 +0700

    Simple Data Binding Final

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget3/AppCompatActivity.java
@@ -0,0 +1,11 @@
+package id.sch.smktelkom_mlg.learn.advancedwidget3;
+
+import android.os.Bundle;
+
+/**
+ * Created by dita ayu on 11/1/2016.
+ */
+public class AppCompatActivity {
+    public void onCreate(Bundle savedInstanceState) {
+    }
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget3/DataBindingUtil.java
@@ -0,0 +1,11 @@
+package id.sch.smktelkom_mlg.learn.advancedwidget3;
+
+import id.sch.smktelkom_mlg.learn.advancedwidget3.databinding.ActivityMainBinding;
+
+/**
+ * Created by dita ayu on 11/1/2016.
+ */
+public class DataBindingUtil {
+    public static ActivityMainBinding setContentView(MainActivity mainActivity, int activity_main) {
+    }
+}
